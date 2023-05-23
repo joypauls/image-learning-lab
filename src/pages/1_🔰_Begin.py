09efe8a6dcc9c7982f15ca162e9474df581eff14
joypauls <joypaulsen3@gmail.com>
2023-05-22 23:52:17 -0500

    messing around with charts

--- src/pages/1_🔰_Begin.py
+++ src/pages/1_🔰_Begin.py
@@ -23,6 +23,24 @@ st.image(image.data)
 
 st.image(image.to_grayscale())
 
+x, y = np.meshgrid(range(0, 20), range(0, 20))
+z = image.to_grayscale()[615:635,950:970]
+
+# Convert this grid to columnar data expected by Altair
+source = pd.DataFrame({'x': x.ravel(),
+                     'y': y.ravel(),
+                     'z': z.ravel()})
+
+st.altair_chart(
+alt.Chart(source).mark_rect().encode(
+    x='x:O',
+    y='y:O',
+    color='z:Q'
+)
+)
+
+# st.image(image.to_grayscale()[615:635,950:970])
+
 # progress_bar = st.sidebar.progress(0)
 # status_text = st.sidebar.empty()
 # last_rows = np.random.randn(1, 1)
